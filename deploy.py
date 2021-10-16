32c42869b8a0210ae473c29e997ed2a8f668304a
david-ajax <79573368+david-ajax@users.noreply.github.com>
2021-10-17 00:50:36 +0800

    Add files via upload

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 import codecs, markdown, os
+os.system("cd files && find > ../find.log")
 def readmemd():
     # 读取 markdown 文本
     input_file = codecs.open("README.md", mode="r", encoding="utf-8")
@@ -26,9 +27,9 @@ def get_filelist():
             Filelist.append(line.rstrip())
     return Filelist
 def create(go):
+    sp = os.getcwd()
     os.chdir(go)
     rewrite_file("index.html", o_file)
-    print("hey")
     filetext = ""
     ld = os.listdir()
     ld.remove("index.html")
@@ -36,32 +37,31 @@ def create(go):
         if os.path.isdir(one) == True:
             plus = "/"
             filetext = filetext + '<p><a href="' + one + '">' + one + plus + '</a><p>' + "\n"
-            print(filetext)
         else:
             plus = ""
             filetext = filetext +  '<p><a href="' + one + '" download="'  + one + '">' + one + plus + '</a><p>' + "\n"
-    replace("index.html", r"$py_file_path" , os.getcwd())
+    replace("index.html", r"$py_file_path" , os.path.relpath(".",static_path+"/files"))
     replace("index.html", r"$py_file_list" ,filetext)
     if os.path.exists("README.md") == True and os.path.isfile("README.md") == True:
         replace("index.html", r"$py_file_readme" ,readmemd())
     else:
         replace("index.html", r"$py_file_readme" ,"暂无内容")
-    os.chdir(static_path)
+    os.chdir(sp)
 def main():
     filel = get_filelist()
     folderl = []
     for ph in filel:
-        print(ph)
         if os.path.isdir(ph) == True:
             folderl.append(ph)
+    print(folderl)
     for go in folderl:
+        print(os.getcwd())
         create(go)
+        print("did")
     print("Completed")
 o_file = read_file("./source/index.html")
 os.chdir(".")
 static_path = os.getcwd()
-print(get_filelist())
-os.system("find > find.log")
 os.system("cd files && rm -rf $(find -name index.html) && cd ..")
 os.chdir("files")
 main()
---
+++ files/README.md
@@ -0,0 +1,5 @@
+## 所有公开文件的根目录
+虽然所呈现的URL为 http(s)://box.wangzhiyu.ga/files/ 但此处应表示为"文件根目录"(即"/")  
+e.g. 在根目录的Demo文件夹中有一个demo.txt文件  
+则链接为 http(s)://box.wangzhiyu.ga/files/Demo/demo.txt  
+而非 http(s)://box.wangzhiyu.ga/Demo/demo.txt  
