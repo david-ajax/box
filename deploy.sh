ba7255f95b25463ed578230f962f64b5e167f8f3
david-ajax <79573368+david-ajax@users.noreply.github.com>
2022-01-02 00:27:49 +0800

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-uname > uname.log
-du files -ah --max-depth=0 > storage.log
-cd files && find > ../find.log && cd ..
-cd files && rm -rf $(find -name index.html) && cd ..
-date > date.log
-python3 main.py > deploy.log
